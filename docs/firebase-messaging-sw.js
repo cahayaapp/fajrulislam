e2627ddf6e5cb730a6643b6c1f138fa3741d5070
cahayaapp <cahayafajrulislam@gmail.com>
2026-09-04 11:14:16 +0700

    1

--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-/* CAHAYA APP v132 — PWA + Firebase Messaging */
-const CAHAYA_SW_BUILD='v132';
+/* CAHAYA APP v134 — PWA + Firebase Messaging */
+const CAHAYA_SW_BUILD='v134';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim());});
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
